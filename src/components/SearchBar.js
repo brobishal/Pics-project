db25bc0a55df02eadbb2a430fff61a8e470a0db2
Bishal Shrestha <bbrobishal@gmail.com>
2023-05-14 19:29:06 +0545

    pics project

---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// class based component
+class SearchBar extends React.Component{
+    render(){
+        return(
+            <div>
+                Search bar
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
