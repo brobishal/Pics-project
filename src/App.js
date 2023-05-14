5346a8ab52bfe788b137e4a47977aebd8b3b20bc
Bishal Shrestha <bbrobishal@gmail.com>
2023-05-14 19:59:54 +0545

    adding touch semantic styling

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import SearchBar from './components/SearchBar';
 const App = () =>{
   return(
-    <>
-      <SearchBar/>
-    </>
-  )
+    <div className="ui container" style={{marginTop:"10px"}}>
+        <SearchBar/>
+    </div>
+  );
 }
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,8 +4,13 @@ import React from 'react';
 class SearchBar extends React.Component{
     render(){
         return(
-            <div>
-                Search bar
+            <div className='ui segment'>
+                <form className='ui form'>
+                <div className='field'>
+                <label>Image Search</label>
+                <input type="text"/>
+                </div>
+                </form>
             </div>
         )
     }
