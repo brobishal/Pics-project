2aecdec94d079c1f594ce5fb400ee4ac4e0db36e
Bishal Shrestha <bbrobishal@gmail.com>
2023-05-17 09:09:06 +0545

    invoking callback function in children

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import SearchBar from './components/SearchBar';
-const App = () =>{
-  return(
-    <div className="ui container" style={{marginTop:"10px"}}>
-        <SearchBar/>
-    </div>
-  );
+import React from "react";
+import SearchBar from "./components/SearchBar";
+
+// step 1 refactoring functional component to class based component
+
+class App extends React.Component {
+  // define callback function on app 
+  onSearchSubmit(term){
+    console.log(term);
+
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmitData={this.onSearchSubmit}/>
+      </div>
+    );
+  }
 }
+
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,19 +1,73 @@
-import React from 'react';
+import React from "react";
 
 // class based component
-class SearchBar extends React.Component{
-    render(){
-        return(
-            <div className='ui segment'>
-                <form className='ui form'>
-                <div className='field'>
-                <label>Image Search</label>
-                <input type="text"/>
-                </div>
-                </form>
-            </div>
-        )
-    }
+class SearchBar extends React.Component {
+  // we initialize the state object with epmty
+  state = { term: "" };
+  // onInputChange = (event) => {
+  //   // if some type in this input this onInputChange callback function will be called
+  //   // this call back has one parameter automatically
+  //   console.log(event.target.value);
+  // };
+  //I am going to define new call back
+  //   onInputClick = () => {
+  //     console.log("input was clicked");
+  //   };
+
+  // second way to solve the problem
+  // used arrow function insted anonymous function
+  //the features of arrow function is that automatially bind the value of this for all code inside of this function
+
+  onFormSubmit(event) {
+    event.preventDefault();
+    // console.log(this.state.term);
+
+    // one little issue here everytime we have made to props
+    //it always been a functional component so here is a class based component
+
+    // one little difference between class based component and function based component that is
+    //we this.props in class component that means we use this before props but in functional we use pros.
+
+    // takes the props object and call the function and pass data
+    this.props.onSubmitData(this.state.term);
+  }
+
+  //making like this another ways
+  // {/* <form className="ui form" onSubmit={(event) => this.onFormSubmit(event)}> */}
+
+  render() {
+    return (
+      <div className="ui segment">
+        {/* we just passed arr function directly */}
+        {/* <form className="ui form" onSubmit={(event) => this.onFormSubmit(event)}> */}
+        {/* here arrow function class at time */}
+        {/* <form className="ui form" onSubmit={event => this.onFormSubmit(event)}> */}
+        <form
+          className="ui form"
+          onSubmit={(event) => this.onFormSubmit(event)}
+        >
+          <div className="field">
+            <label>Image Search</label>
+
+            {/* this is uncontrolled form element so u and i as a react developer prefer to work with control component
+             */}
+            {/* <input type="text" onChange={this.onInputChange} /> */}
+            {/* rather than passing an reference to onInputChange */}
+            {/* alternative way to replacing with arrow function*/}
+            {/* <input type="text" onChange={(e)=>console.log(e.target.value)} /> */}
+
+            {/* value={this.state.term} is equal to */}
+            {/* this is control element */}
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={(e) => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
